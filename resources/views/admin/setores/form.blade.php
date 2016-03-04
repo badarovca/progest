019d55c7a8d7f71381252c316ca90eda34088139
Danilo Lopes <danilo.1.lps@gmail.com>
2016-03-04 10:13:15 -0300

    Configurações de email, correção form setores

--- resources/views/admin/setores/form.blade.php
+++ resources/views/admin/setores/form.blade.php
@@ -12,10 +12,6 @@
         </div>
     </div>
     <div class="row">
-        <div class='col-md-6'>
-            {!!Form::label('coordenacao_id', 'Coordenação', array('class'=>'control-label'))!!}
-            {!!Form::select('coordenacao_id', $coordenacoes, null, ['required' => 'required', 'class' => 'form-control', 'id'=>'coordenacao_id'])!!}
-        </div>
         <div class='checkbox col-md-4' >
             <label>
                 {!!Form::checkbox('status', null)!!} Ativado
