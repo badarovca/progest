31c60a09b575038b6777204e8877569472570011
Danilo Lopes <danilo.1.lps@gmail.com>
2016-04-11 12:21:04 -0300

    Relatorio contabil exibe somente linhas que nao estejam zeradas.

--- app/progest/repositories/RelatorioRepository.php
+++ app/progest/repositories/RelatorioRepository.php
@@ -44,71 +44,73 @@ class RelatorioRepository {
 
     public function getRelatorioContabil($input) {
         $periodo = [date("Y-m-d", strtotime($input['ano'] . "-" . $input['mes'] . "-01")), date("Y-m-t", strtotime($input['ano'] . "-" . $input['mes'] . "-01"))];
-//        dd($periodo);
         $mesAnterior = date("m", strtotime($periodo[0] . "-1 month"));
         $anoAnterior = date("Y", strtotime($periodo[0] . "-1 month"));
         $result = \DB::select(\DB::raw("
-            select entradas.*, saidas.vl_saida, devolucoes.vl_devolucao, 
-            saldos_finais.vl_saldo_final, saldos_iniciais.vl_saldo_inicial from
-            (select sub_items.id, sub_items.material_consumo,
+            select id, material_consumo, sum(vl_entrada) as vl_entrada, sum(vl_saida) as vl_saida, sum(vl_devolucao) as vl_devolucao, 
+            sum(vl_saldo_inicial) as vl_saldo_inicial, sum(vl_saldo_final) as vl_saldo_final
+            from(
+            (select sub_items.id, sub_items.material_consumo, 
             SUM(ROUND(sub_materials.vl_total/sub_materials.qtd_solicitada, 2)*entrada_sub_material.quant)
-            as vl_entrada
+            as vl_entrada, null as vl_saida, null as vl_devolucao, null as vl_saldo_inicial, null as vl_saldo_final
             from sub_items
             left join materials
             on sub_items.id = materials.sub_item_id
             left join sub_materials
             on materials.id = sub_materials.material_id
-            left join entrada_sub_material
-            on sub_materials.id = entrada_sub_material.sub_material_id and (entrada_sub_material.created_at between '" . $periodo[0] . "' and '" . $periodo[1] . "')
-            left join entradas
+            right join entrada_sub_material
+            on sub_materials.id = entrada_sub_material.sub_material_id
+            right join entradas
             on entrada_sub_material.entrada_id = entradas.id 
-            group by sub_items.id) entradas
-            left join 
-            (select sub_items.id, sub_items.material_consumo,
-            SUM(ROUND(sub_materials.vl_total/sub_materials.qtd_solicitada, 2)*saida_sub_material.quant)
-            as vl_saida
+            where (entrada_sub_material.created_at between '" . $periodo[0] . "' and '" . $periodo[1] . "')
+            group by sub_items.id)
+            union all
+            (select sub_items.id, sub_items.material_consumo, 
+            null as vl_entrada, SUM(ROUND(sub_materials.vl_total/sub_materials.qtd_solicitada, 2)*saida_sub_material.quant)
+            as vl_saida, null as vl_devolucao, null as vl_saldo_inicial, null as vl_saldo_final
             from sub_items
             left join materials
             on sub_items.id = materials.sub_item_id
             left join sub_materials
             on materials.id = sub_materials.material_id
-            left join saida_sub_material
-            on sub_materials.id = saida_sub_material.sub_material_id and (saida_sub_material.created_at between '" . $periodo[0] . "' and '" . $periodo[1] . "')
-            left join saidas
+            right join saida_sub_material
+            on sub_materials.id = saida_sub_material.sub_material_id
+            right join saidas
             on saida_sub_material.saida_id = saidas.id 
-            group by sub_items.id) saidas
-            on entradas.id = saidas.id
-            left join
-            (select sub_items.id, sub_items.material_consumo,
-            SUM(ROUND(sub_materials.vl_total/sub_materials.qtd_solicitada, 2)*devolucao_sub_material.quant)
-            as vl_devolucao
+            where (saida_sub_material.created_at between '" . $periodo[0] . "' and '" . $periodo[1] . "')
+            group by sub_items.id)
+            union all
+            (select sub_items.id, sub_items.material_consumo, 
+            null as vl_entrada, null as vl_saida, SUM(ROUND(sub_materials.vl_total/sub_materials.qtd_solicitada, 2)*devolucao_sub_material.quant) as vl_devolucao, 
+            null as vl_saldo_inicial, null as vl_saldo_final
             from sub_items
             left join materials
             on sub_items.id = materials.sub_item_id
             left join sub_materials
             on materials.id = sub_materials.material_id
-            left join devolucao_sub_material
-            on sub_materials.id = devolucao_sub_material.sub_material_id and (devolucao_sub_material.created_at between '" . $periodo[0] . "' and '" . $periodo[1] . "')
-            left join devolucaos
-            on devolucao_sub_material.devolucao_id = devolucaos.id 
-            group by sub_items.id) devolucoes
-            on saidas.id = devolucoes.id
-            left join
-            (select sub_items.id, sub_items.material_consumo, 
-            saldos.valor as vl_saldo_inicial
+            right join devolucao_sub_material
+            on sub_materials.id = devolucao_sub_material.sub_material_id
+            right join devolucaos
+            on devolucao_sub_material.devolucao_id = devolucaos.id
+            where (devolucao_sub_material.created_at between '" . $periodo[0] . "' and '" . $periodo[1] . "')
+            group by sub_items.id) 
+            union all
+            (select sub_items.id, sub_items.material_consumo,
+            null as vl_entrada, null as vl_saida, null as vl_devolucao, saldos.valor as vl_saldo_inicial, null as vl_saldo_final 
             from sub_items
-            left join saldos
-            on sub_items.id = saldos.sub_item_id and saldos.mes = '" . $mesAnterior . "' and saldos.ano = '" . $anoAnterior . "'
-            group by sub_items.id) saldos_iniciais
-            on devolucoes.id = saldos_iniciais.id
-            left join
+            right join saldos
+            on sub_items.id = saldos.sub_item_id
+            where saldos.mes = '" . $mesAnterior . "' and saldos.ano = '" . $anoAnterior . "' and saldos.valor != 0
+            group by sub_items.id)
+            union all
             (select sub_items.id, sub_items.material_consumo, 
-            saldos.valor as vl_saldo_final
+            null as vl_entrada, null as vl_saida, null as vl_devolucao, null as vl_saldo_inicial, saldos.valor as vl_saldo_final
             from sub_items
-            left join saldos
-            on sub_items.id = saldos.sub_item_id and saldos.mes = '" . $input['mes'] . "' and saldos.ano = '" . $input['ano'] . "'
-            group by sub_items.id) saldos_finais
-            on saldos_iniciais.id = saldos_finais.id;"));
+            right join saldos
+            on sub_items.id = saldos.sub_item_id
+            where saldos.mes = '" . $input['mes'] . "' and saldos.ano = '" . $input['ano'] . "' and saldos.valor != 0
+            group by sub_items.id)) rel
+            group by id;"));
         return collect($result);
     }
 
