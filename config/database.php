70c204c425784ca45e39c50e372f75c0c2c17167
Danilo Lopes Santos <danilo.1.lps@gmail.com>
2017-07-24 17:41:34 -0300

    Update database.php

--- config/database.php
+++ config/database.php
@@ -54,10 +54,10 @@ return [
 
 		'mysql' => [
 			'driver'    => 'mysql',
-			'host'      => env('DB_HOST', 'mysql01.ifba.edu.br'),
+			'host'      => env('DB_HOST', '127.0.0.1'),
 			'database'  => env('DB_DATABASE', 'progest'),
-			'username'  => env('DB_USERNAME', 'progest'),
-			'password'  => env('DB_PASSWORD', 'aF765NLtr2Ztm8CE'),
+			'username'  => env('DB_USERNAME', 'root'),
+			'password'  => env('DB_PASSWORD', ''),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
